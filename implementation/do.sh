a1d1fcbc1a7ceca450173e536015ca0c0cf6a6e2
Kaleb Himes <kaleb@wolfssl.com>
2014-10-26 14:28:48 -0600

    re-arranged libraries, autoconf done, look at errors on make

--- implementation/do.sh
+++ implementation/do.sh
@@ -1,15 +1,15 @@
 javac *.java
-javah JavaWinChecker
+javah -d libs JavaWinChecker
 #in the AI directory
 cd AI
 javac *.java
-javah JavaTDNN
-javah JavaClassifier
+javah -d ../libs JavaTDNN
+javah -d ../libs JavaClassifier
 cd ..
 #back in the implementation directory
 #/ creates function prototype for the "pass" method /#
 javac gui/Main.java
 #/ create the header /#
-javah gui.JavaPasser
+javah -d libs gui.JavaPasser
 #/ compile the main program /#
 make
